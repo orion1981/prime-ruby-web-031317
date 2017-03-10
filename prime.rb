26ed2e8036f2f576fd29070004e0291b94168746
charlie stopek <cstopek@hotmail.com>
2017-03-10 22:40:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  # take number passed as parameter
+  # create from (2...number)
+    # if number % i != 0
+      # return true
+    # else
+        # false
+        return false if number.abs < 2
+    i = number.abs - 1
+    while i >= 2
+      return false if number % i == 0
+      i -= 1
+    end
+    true
+end
